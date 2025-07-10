75bf9c518fad084c854c2b4659411d55c728d73f
140bpmubstep <laineprikol@gmail.com>
2025-07-10 13:25:59 +0300

    View

--- footcomp-ui/src/lib.rs
+++ footcomp-ui/src/lib.rs
@@ -1,10 +1,10 @@
 #![no_std]
 extern crate alloc;
 
-use core::fmt::Debug;
+use core::{fmt::Display, time::Duration};
 
-use alloc::string::ToString;
-use chrono::{DateTime, NaiveDate, NaiveDateTime, TimeZone, Utc};
+use alloc::{fmt, format, string::ToString};
+use chrono::DateTime;
 use edgy::{
     embedded_graphics::{
         mono_font::ascii::{FONT_4X6, FONT_5X8},
@@ -22,40 +22,61 @@ use edgy::{
     },
 };
 
-use crate::{style::BATTERY_INDICATOR_STYLE, widgets::tabbar::TabBarLabel};
+use crate::{style::BATTERY_INDICATOR_STYLE};
 
 pub mod style;
 pub mod widgets;
 
-pub fn main_ui<'a, D: DrawTarget<Color = BinaryColor> + 'a>() -> WidgetObject<'a, D, BinaryColor> {
-    let mut ui = LinearLayoutBuilder::default()
-        .vertical_alignment(LayoutAlignment::Center)
-        .horizontal_alignment(LayoutAlignment::Center)
-        .direction(LayoutDirection::Horizontal);
+pub struct FormatTime(pub Duration);
 
-    ui.add_widget_obj(widgets::small_seven_segment_text("0000", "ODO km"));
+impl Display for FormatTime {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let total_secs = self.0.as_secs();
+        let hours = total_secs / 3600;
+        let mins = (total_secs % 3600) / 60;
+        let secs = total_secs % 60;
+        write!(f, "{:02}:{:02}:{:02}", hours, mins, secs)
+    }
+}
 
-    let mut speed = LinearLayoutBuilder::default()
-        .vertical_alignment(LayoutAlignment::Center)
-        .horizontal_alignment(LayoutAlignment::Center)
-        .direction(LayoutDirection::Vertical);
+#[derive(Default)]
+struct DisplayPage {
+    pub speed: u8,
+    pub trip_km: u16,
+    pub ride_time: Duration,
+}
 
-    let style = SevenSegmentStyleBuilder::new()
-        .digit_size(Size::new(16, 32))
-        .segment_width(4)
-        .digit_spacing(4)
-        .segment_color(BinaryColor::On)
-        .build();
+impl DisplayPage {
+    fn draw<'a, D: DrawTarget<Color = BinaryColor> + 'a>(&self) -> WidgetObject<'a, D, BinaryColor> {
+        let mut ui = LinearLayoutBuilder::default()
+            .vertical_alignment(LayoutAlignment::Center)
+            .horizontal_alignment(LayoutAlignment::Center)
+            .direction(LayoutDirection::Horizontal);
 
-    speed.seven_segment("50", style);
-    speed.label("km/h", text::Alignment::Center, &FONT_5X8);
+        ui.add_widget_obj(widgets::small_seven_segment_text(format!("{:0>4}", self.trip_km), "TRIP km"));
 
-    ui.margin_layout(margin!(0, 6), |ui| {
-        ui.add_widget_obj(speed.finish());
-    });
-    ui.add_widget_obj(widgets::small_seven_segment_text("00:00", "TIME"));
+        let mut speed = LinearLayoutBuilder::default()
+            .vertical_alignment(LayoutAlignment::Center)
+            .horizontal_alignment(LayoutAlignment::Center)
+            .direction(LayoutDirection::Vertical);
 
-    ui.finish()
+        let style = SevenSegmentStyleBuilder::new()
+            .digit_size(Size::new(16, 32))
+            .segment_width(4)
+            .digit_spacing(4)
+            .segment_color(BinaryColor::On)
+            .build();
+
+        speed.seven_segment(format!("{:0>2}", self.speed), style);
+        speed.label("km/h", text::Alignment::Center, &FONT_5X8);
+
+        ui.margin_layout(margin!(0, 6), |ui| {
+            ui.add_widget_obj(speed.finish());
+        });
+        ui.add_widget_obj(widgets::very_small_seven_segment_text(format!("{}", FormatTime(self.ride_time)), "TIME"));
+
+        ui.finish()
+    }
 }
 
 pub fn base_ui<'a, D: DrawTarget<Color = BinaryColor> + 'a>() -> WidgetObject<'a, D, BinaryColor> {
@@ -68,19 +89,20 @@ pub fn base_ui<'a, D: DrawTarget<Color = BinaryColor> + 'a>() -> WidgetObject<'a
         .add_column(100);
 
     let ts: i64 = 1_720_000_000;
-
     let nt = DateTime::from_timestamp(ts, 0).unwrap();
+
+    let mut display_page = DisplayPage::default();
+
+    display_page.speed = 2;
+    display_page.trip_km = 401;
+    display_page.ride_time = Duration::from_secs(5940 + 59);
 
     main_grid.horizontal_linear_layout(LayoutAlignment::Stretch, |ui| {
         ui.label(
-            nt.format("%H:%M").to_string(),
+            nt.format("%H:%M Jul 8").to_string(),
             text::Alignment::Left,
             &FONT_4X6,
         );
-
-        ui.horizontal_linear_layout(LayoutAlignment::Center, |ui| {
-            ui.label("Tue Jul 2", text::Alignment::Center, &FONT_4X6);
-        });
 
         ui.horizontal_linear_layout(LayoutAlignment::End, |ui| {
             let bat_style = BatteryStyle::new(BATTERY_INDICATOR_STYLE, LayoutDirection::Horizontal);
@@ -88,13 +110,10 @@ pub fn base_ui<'a, D: DrawTarget<Color = BinaryColor> + 'a>() -> WidgetObject<'a
         });
     });
 
-    main_grid.add_widget_obj(main_ui());
+    main_grid.add_widget_obj(display_page.draw());
 
     main_grid.horizontal_linear_layout(LayoutAlignment::Start, |ui| {
-        ui.margin_layout(margin!(0, 3, 0, 0), |ui| {
-            ui.add_widget(TabBarLabel::new(true, "DISPLAY"));
-        });
-        ui.add_widget(TabBarLabel::new(false, "LOG"));
+        ui.label("OAT: 28C", text::Alignment::Left, &FONT_5X8);
     });
 
     margin_layout.add_widget_obj(main_grid.finish());
--- footcomp-ui/src/widgets/mod.rs
+++ footcomp-ui/src/widgets/mod.rs
@@ -1,6 +1,11 @@
 use alloc::string::String;
 use edgy::{
-    embedded_graphics::{mono_font::ascii::FONT_5X8, pixelcolor::BinaryColor, prelude::*, text},
+    embedded_graphics::{
+        mono_font::ascii::{FONT_4X6, FONT_5X8},
+        pixelcolor::BinaryColor,
+        prelude::*,
+        text,
+    },
     prelude::*,
     widgets::{
         label::{Label, LabelOptions, SevenSegmentStyleBuilder},
@@ -28,9 +33,35 @@ pub fn small_seven_segment_text<'a, D: DrawTarget<Color = BinaryColor> + 'a, S: 
     layout.seven_segment(text, style);
     layout.add_widget(Label::new(
         heading_text,
-        LabelOptions::new()
-            .alignment(text::Alignment::Center)
-            .line_height(0),
+        LabelOptions::new().alignment(text::Alignment::Center),
+        &FONT_5X8,
+    ));
+    layout.finish()
+}
+
+pub fn very_small_seven_segment_text<
+    'a,
+    D: DrawTarget<Color = BinaryColor> + 'a,
+    S: Into<String>,
+>(
+    text: S,
+    heading_text: &str,
+) -> WidgetObject<'a, D, BinaryColor> {
+    let style = SevenSegmentStyleBuilder::new()
+        .digit_size(Size::new(5, 16))
+        .segment_width(1)
+        .digit_spacing(1)
+        .segment_color(BinaryColor::On)
+        .build();
+
+    let mut layout = LinearLayoutBuilder::default()
+        .direction(LayoutDirection::Vertical)
+        .horizontal_alignment(LayoutAlignment::Center);
+
+    layout.seven_segment(text, style);
+    layout.add_widget(Label::new(
+        heading_text,
+        LabelOptions::new().alignment(text::Alignment::Center),
         &FONT_5X8,
     ));
     layout.finish()
